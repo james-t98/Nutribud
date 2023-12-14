ccd3485ad5c6cb42b548b74d5e6454c2c7d807c8
u17021627 <u17021627@tuks.co.za>
2023-12-14 16:23:26 +0100

    Initial app components added

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FolderPage } from './folder.page';
+import { LoginFormComponent } from '../login-form/login-form.component';
 
 const routes: Routes = [
   {
